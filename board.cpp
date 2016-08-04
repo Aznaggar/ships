2333c2c807183dacca2d5a0c2efe19d0bfc51bf0
Aznaggar <timekenz@gmail.com>
2016-08-04 14:34:41 +0200

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -1,21 +1,62 @@
 #include "board.h"
+#include <iostream>
+
+using namespace std;
 
 Board::Board()
 {
-    int* ptr = new int[100];
+    int** ary = new int*[10];
+    for(int i = 0; i < 10; ++i)
+    {
+        ary[i] = new int[10];
+    }
+    cout << "c-tor working..." <<endl;
 }
 
 Board::~Board()
 {
-    delete ptr;
+    for(int i = 0; i < 10; ++i)
+    {
+        delete [] ary[i];   // przy zmianie na delete ary[i]
+    }                       // i 
+    delete [] ary;          // delete ary destruktor nie dziala
+    cout << "d-tor working..."<<endl;
 }
 
-int Board::SetBoard()
+Board::Board(const Board& other)
 {
-
+    //**ary=other.ary;
 }
 
-void Board::GetBoard()
+void Board::fillZeros()
+{
+    for (int i=0; i<10; i++)
+    {
+        for(int j=0; j<10; j++)
+        {
+            ary[i][j]=0;
+        }
+    }
+}
+
+void Board::PrintBoard()
+{
+    for (int i=0; i<10; i++)
+    {
+        for(int j=0; j<10; j++)
+        {
+            cout<<ary[i][j];
+        }
+        cout<<endl;
+    }
+}
+
+int Board::GetBoard()
+{
+    return (int)ary;
+}
+
+void Board::SetBoard()
 {
 
 }
